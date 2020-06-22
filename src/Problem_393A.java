e36f63498a8836f4d15fe0cb98ab39c24366c09e
sivajik <sivaji_kondapalli@yahoo.com>
2020-06-22 17:48:47 +0530

    Initial Commit

---
+++ src/Problem_393A.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Problem_393A {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		char[] in =  input.toCharArray();
+		int hash[] = new int[256]; 
+		for(int i = 0 ; i<in.length ; i++){
+			hash[in[i]]++;
+		}
+		int n = (hash['n']-1)/2;
+		int i = hash['i'];
+		int t = hash['t'];
+		int e = hash['e']/3;
+		System.out.println(Math.min(Math.min(n,i), Math.min(t, e)));
+	}
+}
